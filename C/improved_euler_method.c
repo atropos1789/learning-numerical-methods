102e0360f4d09e41b07d3f2e40ecc262343e4496
atropos1789 <128442611+atropos1789@users.noreply.github.com>
2023-10-13 11:44:51 -0400

    Fixed memory overflow bug in euler_method.c and added improved_euler_method.c

--- C/improved_euler_method.c
+++ C/improved_euler_method.c
@@ -1,4 +1,11 @@
-#include <stdio.h>    
+#include <stdio.h>
+
+/*
+The Improved Euler Method for approximating the solution to the IVP 
+y' = f(t,y), y(t_0) = y_0
+is
+y_{n+1} = y_n + 0.5h( f(t_n, y_n) + f(t_n + h, y_n + h*f(t_n,y_n) ) )
+*/
 
 double function(double t, double y) { 
 
@@ -11,37 +18,27 @@ double function(double t, double y) {
     return func_value;
 };
 
-/*
-The Euler Method for approximating the solution to the IVP 
-y' = f(t,y), y(t_0) = y_0
-is
-y_{n+1} = y_n + h*f(t_n, y_n)
-*/
-
-int forward_euler(double (*fn) (double, double), double T_INITIAL, double Y_INITIAL, double STEP_SIZE, int STEPS) {
+int improved_euler(double (*fn) (double, double), double T_INITIAL, double Y_INITIAL, double STEP_SIZE, int STEPS) {
 
     double domain[STEPS+1], output[STEPS+1];
     domain[0] = T_INITIAL;
     output[0] = Y_INITIAL;
     
-    for (int j = 1; j <= STEPS; j++) {
+    for (int j = 1; j < (STEPS+1); j++) {
         domain[j] = T_INITIAL + j*STEP_SIZE;
-        // printf("STEP: %f\n",domain[j]);
     };
     
     printf("(%f, %f)\n",  domain[0], output[0]);
 
     for ( int i = 0; i < STEPS; i++) {
-        output[i+1] = output[i] + fn(domain[i], output[i])*STEP_SIZE;
+        double h1 = fn(domain[i], output[i]);
+        output[i+1] = output[i] + 0.5*STEP_SIZE*(h1 + fn(domain[i] + STEP_SIZE, output[i] + STEP_SIZE*h1 ));
         printf("(%.17f , %.17f)\n", domain[i+1], output[i+1]);
-        // printf("%.17f\n", domain[i+1]);
     };
     
     return 0;
 };
 
 int main(void) {
-    forward_euler(function, 0.0, 0.0, 0.01, 1000);
-
-    return 0;
+    improved_euler(function, 0.0, 0.0, 0.01, 1000);
 };
